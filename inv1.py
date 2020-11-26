92c100d6c0c001ffec08e179bcce1ca241c5915a
Uva337 <uvarov.nik2016@yandex.ru>
2020-11-26 21:14:29 +0300

    invidual

---
+++ inv1.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == '__main__':
+    A = list(map(int, input().split()))
+    if len(A) != 10:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
+    s = 0
+    l = 0
+    for item in A:
+        if abs(item) < 3 and item % 9 == 0:
+            s += item
+            l += 1
+    print(s, l)
